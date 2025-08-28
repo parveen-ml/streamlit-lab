3b0cedddf1cee9bee31c36a4ec7dd4b1a7575dec
parveen-ml <er.parveen44@gmail.com>
2025-08-28 07:21:33 +0530

    chapter 1

--- chapter-one-assignment.py
+++ chapter-one-assignment.py
@@ -5,6 +5,6 @@ st.subheader("How are you doing")
 st.text("Welcome to the app")
 st.write("Choose your fav. programming language")
 
-lang = st.selectbox("Your fav. language: ", ["Python", "Java", "CPP", "JavaScritp"])
+lang = st.selectbox("Your fav. language: ", ["Python", "Java", "CPP", "JavaScript"])
 
 st.success(f"You choose {lang}. Excellent choice")
